2c03d0a4680ca4222e8cd6df2294f3a43e3ec51a
VictorAndresDlh <vadela@unal.edu.co>
2023-10-06 01:05:11 -0500

    changes

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import numpy as np
 # Load the mask image for the word cloud
 mask_image = np.array(Image.open("bogota.png"))
 
+
 # Function to generate and display a word cloud from text data
 def show_wordcloud(data):
     # Create a WordCloud object with specified parameters
@@ -23,7 +24,7 @@ def show_wordcloud(data):
     ).generate(str(data))
 
     # Create a plot of the word cloud
-    fig = plt.figure(1, figsize=(10, 10))
+    plt.figure(1, figsize=(10, 10))
     plt.imshow(wordcloud)
     plt.axis("off")
     plt.margins(x=0, y=0)
@@ -42,6 +43,7 @@ def show_wordcloud(data):
 
 # Load the Spanish language model for spaCy
 nlp = spacy.load("es_core_news_sm")
+
 
 # Function to preprocess text data for the word cloud
 def preprocess_text(text):
@@ -73,8 +75,15 @@ file_names = {
 selected_file = st.sidebar.selectbox("Escoja un candidato", file_names.keys())
 
 # Display the app title and selected candidate
-st.title("Elecciones Bogotá 2023")
-st.subheader(f"¿De qué habla {selected_file}?")
+st.title("Elecciones Bogotá 2023 :city_sunset:")
+st.subheader(
+    "En este dashboard, podrás explorar los temas más importantes de los candidatos a la alcaldía de Bogotá."
+)
+st.divider()
+st.header(f"¿De qué habla {selected_file}?")
+st.markdown(
+    f"A continuación te mostramos las palabras más comunes usadas por el candidato {selected_file} tanto en entrevistas como en debates:"
+)
 
 # Load and preprocess the selected candidate's text data
 with open(file_names[selected_file], "r", encoding="utf-8") as f:
@@ -83,3 +92,5 @@ with open(file_names[selected_file], "r", encoding="utf-8") as f:
 
 # Generate and display a word cloud from the preprocessed text data
 show_wordcloud(f)
+st.divider()
+st.subheader("Siguientes pasos")
