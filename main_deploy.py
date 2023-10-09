4818718497c1a50f5c9567390790574c41a7ed37
VictorAndresDlh <vadela@unal.edu.co>
2023-10-08 21:49:18 -0500

    update main_deploy

--- main_deploy.py
+++ main_deploy.py
@@ -1,7 +1,8 @@
 __import__('pysqlite3')
 import sys
-import subprocess
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+import subprocess
 subprocess.run(["python", "-m", "spacy", "download", "es_core_news_sm"])
 
 import streamlit as st
